4c44abf985e218ff0270998c69d863c477bbfeaf
Gustavo Ovelar <gusove@gmail.com>
2015-06-02 10:43:45 -0300

    Definição da clase modelo VendedorVO.java

---
+++ CRUDSQLLiteAndroid/src/br/com/crudsqlliteandroid/POJO/VendedorVO.java
@@ -0,0 +1,40 @@
+package br.com.crudsqlliteandroid.POJO;
+
+import java.io.Serializable;
+
+public class VendedorVO implements Serializable{
+	public VendedorVO(long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public VendedorVO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long serialVersionUID = 1L;
+	  private long id;
+	  private String nome;
+
+	  public long getId() {
+	      return id;
+	  }
+
+	  public void setId(long id) {
+	      this.id = id;
+	  }
+
+	  public String getNome() {
+	      return nome;
+	  }
+
+	  public void setNome(String value) {
+	      this.nome = value;
+	  }
+	  
+	  // Will be used by the ArrayAdapter in the ListView
+	  @Override
+	  public String toString() {
+	      return nome;
+	  }
+	}
