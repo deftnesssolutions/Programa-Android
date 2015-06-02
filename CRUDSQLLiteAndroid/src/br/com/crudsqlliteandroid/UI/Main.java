858ab6176c90c4bbd3201c9d86d3bf9a60ec9a19
Gustavo Ovelar <gusove@gmail.com>
2015-06-02 13:11:10 -0300

    commit de atualização

--- CRUDSQLLiteAndroid/src/br/com/crudsqlliteandroid/UI/Main.java
+++ CRUDSQLLiteAndroid/src/br/com/crudsqlliteandroid/UI/Main.java
@@ -1,15 +1,77 @@
 package br.com.crudsqlliteandroid.UI;
 
+import br.com.crudsqlliteandroid.UI.R.id;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Main extends Activity {
+public class Main extends Activity implements OnClickListener {
 
+    EditText txtNome;
+	Button btnSair, btnVendedor,btnRegistrar,btnCancelar;
+	public String msgb="Pressione o botão Sair para fechar o aplicativo";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		chamaPrincipal();
 	}
 
+	@Override 
+    public  void onBackPressed ()  {// Bloqueo el boton back do tablet 
+    // fazer algo nas costas. 
+    	Toast.makeText(this, msgb, Toast.LENGTH_SHORT).show();
+    	return ; 
+    }
+	
+	//escucha del evento click de los botones, gracias a que la clase implementa el onClickListener
+	public void onClick(View v) {
+    	
+		if (v.getId() == R.id.btnVendedor)
+		{
+	        	registraVendedor();
+		}
+		if (v.getId()== id.btnExit)
+		{
+				System.exit(0);	
+        }		
+	}
+	//##Medoto para llamar a la tela de Vendedor
+	private void registraVendedor() {
+		try
+        {
+            Intent it = new Intent(this, VendedorUI.class);
+            startActivity(it);//chama a tela e incusão
+        }
+        catch (Exception e) {
+            trace("Erro : " + e.getMessage());
+        }  		
+	}
+
+    public void toast (String msg)
+    {
+        Toast.makeText (getApplicationContext(), msg, Toast.LENGTH_SHORT).show ();
+    } 
+     
+    private void trace (String msg) 
+    {
+        toast (msg);
+    }    
+ 
+	//##Metodo para inflar la tela principal
+	private void chamaPrincipal() {
+		setContentView(R.layout.main);
+		
+		btnVendedor = (Button)findViewById(R.id.btnVendedor);
+    	btnVendedor.setOnClickListener(this);
+    	btnSair = (Button)findViewById(id.btnExit);
+    	btnSair.setOnClickListener((OnClickListener) this);
+	}
 }
+
