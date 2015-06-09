4c31cbb98595e37962bde737fa976f1ca996acd4
Gustavo Ovelar <gusove@gmail.com>
2015-06-09 19:59:42 -0300

    commit backup do projeto

---
+++ Agenda/src/br/com/agenda/POJO/ContatoVO.java
@@ -0,0 +1,50 @@
+package br.com.agenda.POJO;
+
+import java.io.Serializable;
+
+//Classe responsável pelo transporte dos dados entre a 
+//interface(tela) e Banco de Dados
+public class ContatoVO implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private long id;
+  private String nome;
+  private String endereco;
+  private String telefone;
+
+  public long getId() {
+      return id;
+  }
+
+  public void setId(long id) {
+      this.id = id;
+  }
+
+  public String getNome() {
+      return nome;
+  }
+
+  public void setNome(String value) {
+      this.nome = value;
+  }
+
+  public String getEndereco() {
+      return endereco;
+  }
+
+  public void setEndereco(String value) {
+      this.endereco = value;
+  }
+   
+  public String getTelefone() {
+      return telefone;
+  }
+
+  public void setTelefone(String value) {
+      this.telefone = value;
+  }
+  // Will be used by the ArrayAdapter in the ListView
+  @Override
+  public String toString() {
+      return nome;
+  }
+}
